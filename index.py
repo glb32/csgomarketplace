4ca75b63f9db87b24da4e7590fd821b343a2734d
ArmykOliva <krystof.olik@gmail.com>
2020-12-27 18:58:03 +0100

    page

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template,redirect
 
 
 from Authentication import SteamSignIn
@@ -7,22 +7,21 @@ from Authentication import SteamSignIn
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-
-	shouldLogin = request.args.get('login')
+def index():
+	"""shouldLogin = request.args.get('login')
 	if shouldLogin is not None:
 		steamLogin = SteamSignIn()
 		# Flask expects an explicit return on the route.
 		return steamLogin.RedirectUser(steamLogin.ConstructURL('http://localhost:8080/processlogin'))
 
-	return 'Click <a href="/?login=true">to log in</a>'
+	return 'Click <a href="/login">to log in</a>'"""
+	return render_template('index.html')
 
-@app.route('/processlogin')
-def process():
-
-	returnData = request.values
-
+@app.route('/login')
+def login():
 	steamLogin = SteamSignIn()
+	returnData = steamLogin.ConstructURL("http://localhost:8080/")
+	print(returnData)
 	steamID = steamLogin.ValidateResults(returnData)
 
 	print('SteamID returned is: ', steamID)
@@ -31,8 +30,9 @@ def process():
 		return 'We logged in successfully!<br />SteamID: {0}'.format(steamID)
 	else:
 		return 'Failed to log in, bad details?'
+	#return redirect("/")
 
 
 if __name__ == '__main__':
 	os.environ['FLASK_ENV'] = 'development'
-	app.run(host = 'localhost', port = 8080, debug = False)
+	app.run(host = 'localhost', port = 8080, debug = True)
